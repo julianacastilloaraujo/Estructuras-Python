0395589a05eb7121f252665b9d5f4d8f310f4a96
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-02-16 15:29:49 -0500

    Cambios

--- lista.py
+++ lista.py
@@ -1,2 +1,4 @@
 lista = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
 print(lista[2])
+print(lista[-4])
+print(lista[0:3])
---
+++ listaejemplo.py
@@ -0,0 +1,4 @@
+lista = ["Lunes",40,5.67,[1,2,3],True]
+print(len(lista))
+lista.insert(2,3)
+lista.append(6)
