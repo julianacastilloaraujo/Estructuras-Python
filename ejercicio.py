90354ce498c24623943511ce7f0cb4488289e0f3
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-02-16 14:30:13 -0500

    Cambios

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,13 @@
-def vocal(letra):
+def vocal(dato):
     vocales = "aeiou"
-    return letra in vocales
+    return dato in vocales
 
-letra = input("Ingrese el dato: ")
+dato = input("Ingrese el dato: ")
 
-if len(letra) != 1:
+if len(dato) != 1:
     print("Por favor, ingrese un solo un dato.")
 else:
-    if vocal(letra):
+    if vocal(dato):
         print("el dato ingresado es una vocal.")
     else:
         print("el dato ingresada no es una vocal.")
